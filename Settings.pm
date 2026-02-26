7db439335bdd7ae103d5c472c54caef9fbbac10b
Michael Herger <michael@herger.net>
2026-02-26 21:00:11 +0100

    Simplify prefs handling by using existing prefs infrastructure to intialize and validate prefs on input.
    This not only simplifies the Settings code, but also makes sure the same rules get applied no matter what way a pref is changed (CLI etc.).
    
    Fix the radio box labels to make them clickable

--- Settings.pm
+++ Settings.pm
@@ -4,15 +4,8 @@ use strict;
 use base qw(Slim::Web::Settings);
 
 use Slim::Utils::Prefs;
-use Slim::Utils::Log;
 
 my $prefs = preferences('plugin.communityfirmware');
-my $log = logger('player.firmware');
-
-my %defaults = (
-	enable	=> '1',
-	beta	=> '0',
-);
 
 sub name {
 	return 'PLUGIN_COMMUNITY_FIRMWARE';
@@ -23,40 +16,9 @@ sub page {
 }
 
 sub prefs {
-        my @prefs = ($prefs, qw(enable beta));
-
-        return @prefs;
+	my @prefs = qw(enable beta);
+	return ($prefs, @prefs);
 }
 
-sub handler {
-	my ($class, $client, $params) = @_;
-
-	$log->debug("CommunityFirmware->handler() called.");
-	if ($params->{saveSettings}) {
-                # make sure value is not undefined, or it gets re-initialized
-		$prefs->set('enable', $params->{'pref_enable'} ||= '0');
-		$prefs->set('beta', $params->{'pref_beta'} ||= '0');
-
-		$log->debug("CommunityFirmware->saveSettings() enabled " . $prefs->get('enable') . " beta " . $prefs->get('beta'));
-	}
-
-	return $class->SUPER::handler( $client, $params );
-}
-
-sub setDefaults {
-	my $force = shift;
-
-	foreach my $key (keys %defaults) {
-		if (!defined($prefs->get($key)) || $force) {
-			$log->debug("Preference not found setting default value for $key " . $defaults{$key});
-			$prefs->set($key, $defaults{$key});
-		}
-	}
-}
-
-sub init {
-        $log->info("CommunityFirmware->init() Initializing settings");
-        setDefaults(0);
-}
 
 1;
